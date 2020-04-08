9ff10b81764016c65518f69bdcb462c13de3a229
PyColors <patweb44@gmail.com>
2020-04-08 16:26:19 +0200

    Set up project

---
+++ src/index.ts
@@ -0,0 +1,3 @@
+// import './components/root/root.element';
+import './components/app.root.element';
+import './style.css';
---
+++ stories/lit-app-ts.stories.js
@@ -0,0 +1,11 @@
+import { html } from 'lit-html';
+import '../src/lit-app-ts.js';
+
+export default {
+  title: 'lit-app-ts',
+};
+
+export const App = () =>
+  html`
+    <lit-app-ts></lit-app-ts>
+  `;

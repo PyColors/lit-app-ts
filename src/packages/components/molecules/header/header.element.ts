aa928de710875a3e65e4065a5cf055cf5f401da5
PyColors <patweb44@gmail.com>
2020-05-03 23:58:38 +0200

    Setting up menu element

--- src/packages/components/molecules/header/header.element.ts
+++ src/packages/components/molecules/header/header.element.ts
@@ -3,6 +3,8 @@ import '@material/mwc-top-app-bar-fixed';
 import '@material/mwc-icon-button';
 import '@material/mwc-button';
 
+import '../menu/menu.element';
+
 @customElement('header-element')
 export class HeaderElement extends LitElement {
   static get styles() {
@@ -17,10 +19,18 @@ export class HeaderElement extends LitElement {
           font-size: 1.25rem;
           line-height: 2rem;
           letter-spacing: 0.4px;
+          display: inline-block;
         }
+
         mwc-top-app-bar-fixed {
-          --mdc-theme-primary: #fff;
+          --mdc-theme-primary: white;
           --mdc-theme-on-primary: black;
+          box-shadow: 0px 0px 2px -1px rgba(0, 0, 0, 0.2), 0px 0px 3px 0px rgba(0, 0, 0, 0.14),
+            0px 1px 10px 0px rgba(0, 0, 0, 0.12);
+        }
+
+        .nav-bar {
+          text-align: right;
         }
       `,
     ];
@@ -29,14 +39,16 @@ export class HeaderElement extends LitElement {
     return html`
       <mwc-top-app-bar-fixed>
         <div slot="title">Py</div>
-
-        <mwc-icon-button icon="favorite" slot="actionItems"></mwc-icon-button>
-        <div>
-          <!-- content -->
+        <div slot="title">
           <nav class="nav-bar">
             <a href="/">home</a>
             <a href="/about">about</a>
           </nav>
+        </div>
+
+        <div>
+          <!-- content -->
+          <menu-element></menu-element>
         </div>
       </mwc-top-app-bar-fixed>
     `;

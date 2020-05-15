9c4ed4f21f75d12802f61ecf1de98c6a93a1c034
PyColors <patweb44@gmail.com>
2020-05-15 22:55:06 +0200

    Adding Polymer app-layout package

--- src/packages/components/molecules/header/header.element.ts
+++ src/packages/components/molecules/header/header.element.ts
@@ -1,56 +1,31 @@
 import { LitElement, html, customElement, css, TemplateResult } from 'lit-element';
-// import '@material/mwc-top-app-bar-fixed';
-// import '@material/mwc-icon-button';
-// import '@material/mwc-button';
-
-// import '../menu/menu.element';
+import '@polymer/app-layout/app-scroll-effects/app-scroll-effects.js';
+import '@polymer/app-layout/app-layout.js';
+import '../menu/menu.element';
 
 @customElement('header-element')
 export class HeaderElement extends LitElement {
   static get styles() {
     return [
       css`
-        a {
-          text-decoration: none;
-        }
-        [slot='title'] {
-          font-family: 'Helvetica', monospace;
-          -webkit-font-smoothing: antialiased;
-          font-size: 1.25rem;
-          line-height: 2rem;
-          letter-spacing: 0.4px;
-          display: inline-block;
-        }
-
-        mwc-top-app-bar-fixed {
-          --mdc-theme-primary: white;
-          --mdc-theme-on-primary: black;
-          box-shadow: 0px 0px 2px -1px rgba(0, 0, 0, 0.2), 0px 0px 3px 0px rgba(0, 0, 0, 0.14),
-            0px 1px 10px 0px rgba(0, 0, 0, 0.12);
-        }
-
-        .nav-bar {
-          text-align: right;
-        }
+      app-header {
+        background-color: white;
+        --app-header-background-rear-layer: {
+          /* The header is blue when condensed *\/
+          background-color: dark;
+        };
+      }
       `,
     ];
   }
   public render(): TemplateResult {
     return html`
-      <mwc-top-app-bar-fixed>
-        <div slot="title">Py</div>
-        <div slot="title">
-          <nav class="nav-bar">
-            <a href="/">home</a>
-            <a href="/about">about</a>
-          </nav>
-        </div>
-
-        <div>
-          <!-- content -->
-          <menu-element></menu-element>
-        </div>
-      </mwc-top-app-bar-fixed>
+      <app-header reveals>
+        <app-toolbar>
+          <div main-title>Lit-app</div>
+        </app-toolbar>
+      </app-header>
+      <app-drawer id="drawer" swipe-open></app-drawer>
     `;
   }
 }

5da896877c4706f8163c5743fadfe45747ac02d5
PyColors <patweb44@gmail.com>
2020-06-15 21:48:14 +0200

    Add input element

---
+++ src/packages/components/molecules/input/input.element.ts
@@ -0,0 +1,24 @@
+import { LitElement, html, customElement, css, TemplateResult } from 'lit-element';
+
+import '@lion/input/lion-input.js';
+
+@customElement('input-element')
+export class InputEmailElement extends LitElement {
+  static get styles() {
+    return [
+      css`
+        .input {
+          color: #1a2b42;
+          line-height: 32px;
+        }
+      `,
+    ];
+  }
+  public render(): TemplateResult {
+    return html`
+      <lion-input class="input">
+        <label slot="label"> </label>
+      </lion-input>
+    `;
+  }
+}
--- src/packages/components/pages/login/login.page.element.ts
+++ src/packages/components/pages/login/login.page.element.ts
@@ -1,5 +1,6 @@
 import { LitElement, html, customElement, property, TemplateResult } from 'lit-element';
 import '../../molecules/input-email/input.email.element';
+import '../../molecules/input/input.element';
 
 import { style } from './login.page.styles';
 
@@ -15,7 +16,9 @@ export class HomeElement extends LitElement {
     return html`
       <main>
         <h1>Login</h1>
-        <input-email-element />
+        <input-email-element></input-email-element>
+        <br />
+        <lion-input type="password" label="Please enter your password"></lion-input>
       </main>
     `;
   }
